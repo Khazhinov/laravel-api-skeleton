f53e3841d44105f8c2f1fdbc9dd808453e1d5b82
Khazhinov Vladislav <khazhinov@gmail.com>
2023-09-05 17:37:05 +0300

    update infrastructure

--- docker/entrypoint/api.sh
+++ docker/entrypoint/api.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 /app/docker/entrypoint/base.sh
 cd /app/
-php artisan octane:roadrunner --host=0.0.0.0 --port=80 --rpc-port=6001 --rr-config=.rr.api.yaml
+php artisan octane:roadrunner --host=0.0.0.0 --port=80 --rpc-port=6001 --rr-config=.rr.api.yaml --log-level=debug
--- docker/entrypoint/base.sh
+++ docker/entrypoint/base.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd /app/
+[ -f /app/.env ] && rm /app/.env
 php artisan optimize
 php artisan service:init
